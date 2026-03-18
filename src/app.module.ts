9ad91b91db23c1da2327050edcc74db498365f80
Nabil <abubakar.nabil.210@gmail.com>
2026-03-18 15:47:22 +0100

    Feat: Added throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HttpModule],
+  imports: [ConfigModule.forRoot(), HttpModule, ThrottlerModule.forRoot([
+    {
+      ttl: 60000,
+      limit: 5,
+    },
+  ])],
   controllers: [AppController],
   providers: [AppService],
 })
